9f068b63c71fc4f947f4fa7685ac57a7c0af7873
Kyle McKee <mckee.kyle@gmail.com>
2019-03-25 12:07:51 -0400

    Added cors policy that ignores cors.

--- backend/Todos2019/Controllers/ValuesController.cs
+++ backend/Todos2019/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Todos2019.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1", "value2", "lunch time" };
         }
 
         // GET api/values/5
--- backend/Todos2019/Startup.cs
+++ backend/Todos2019/Startup.cs
@@ -25,6 +25,13 @@ namespace Todos2019
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -40,6 +47,7 @@ namespace Todos2019
                 app.UseHsts();
             }
 
+            app.UseCors("MyPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
