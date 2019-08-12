9d19abc9ef67daf14c2a7cc4d60424c31aa88251
wenjen10 <wenjen10@yahoo.com>
2019-08-12 16:48:53 -0400

    created folder css & file style.css

---
+++ parcel-frontend/js/app.js
@@ -0,0 +1,22 @@
+const header = document.querySelector (#header);
+header.innerHTML = 
+`<navclass `
+import apiActions from './api/actions';
+import Values from './components/ToDos';
+import '../css/style.css';
+
+pageBuild()
+
+function pageBuild() {
+    header();
+    footer();
+    navHome();
+    navValues();
+    navToDos();
+
+
+
+    function header(){
+        
+    }
+}
