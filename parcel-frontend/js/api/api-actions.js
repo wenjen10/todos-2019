dad22ede6da7f8138e5655b53d4009d55f0cc90c
wenjen10 <wenjen10@yahoo.com>
2019-08-15 18:41:41 -0400

    added content to app.js

--- parcel-frontend/js/api/api-actions.js
+++ parcel-frontend/js/api/api-actions.js
@@ -2,9 +2,22 @@ function getRequest (location, calback) {
     fetch("https://localhost:44326/api/todos")
         .then(response => response.json())
         .then(jsonData => console.log (jsonData))
-        .catch(err => console.log(err));
+        .catch(err => console.log(err))
+}
+function postRequest(location, requestBody, callback){
+    fetch (location, {
+        method: "POST",
+        body: JSON.stringify(requestBody),
+        headers:{
+            "Content-Type": "application/json"
+        }
+    })
+    .then(response => response.json())
+    .then(jsonData => console.log (jsonData))
+    .catch(err => console.log(err))
 }
 
 export default {
     getRequest
+    postRequest
 }
